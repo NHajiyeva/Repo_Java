58232c64a1fc51f8505022832761670e70c3e63b
Nazrin <nazrinhajiyevaa@gmail.com>
2021-02-23 20:50:16 -0500

    HomeWork2

---
+++ src/Class1/MyHomeWork1.java
@@ -0,0 +1,34 @@
+package Class1;
+
+public class MyHomeWork1 {
+    public static void main(String[] args) {
+
+System.out.println("Hello!");
+
+
+        byte numOfhours = 24;
+                System.out.println("Number of hours in one day is " + numOfhours);
+                short numOfdays = 366;
+                        System.out.println("The maximum number of days in a year " + numOfdays);
+                int numOfEmpl = 575700;
+                        System.out.println("The total number of employees in Amazon is " + numOfEmpl);
+                long populOfItaly = 60461826L;
+                        System.out.println("Population of Italy is " + populOfItaly);
+                float intRate = 0.5f;
+                System.out.println("Interest Rate on savings account is " + intRate);
+double accBalance = 100000.55;
+System.out.println("The balance in my bank account is " + accBalance);
+boolean sunRise = false;
+System.out.println("Is it true that the Sun rises from the West? "+ "No, it is " + sunRise);
+char firstName = 'N';
+        char lastName = 'H';
+                System.out.println("Initials of my name is " + firstName+lastName);
+
+
+
+
+    }
+}
+
+
+
